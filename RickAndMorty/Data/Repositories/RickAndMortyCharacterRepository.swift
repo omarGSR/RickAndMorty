28dc7c1251ba386b959f71e97a7a517f2f71dae4
Omar Fazal <silen08620@gmail.com>
2026-05-03 23:45:21 +0200

    RM-4 - update repository to include persistence

--- RickAndMorty/Data/Repositories/RickAndMortyCharacterRepository.swift
+++ RickAndMorty/Data/Repositories/RickAndMortyCharacterRepository.swift
@@ -8,21 +8,26 @@
 final class RickAndMortyCharacterRepository: CharacterRepository {
     
     private let remoteDataSource: CharacterRemoteDataSource
+    private let localDataSource: PersistenceDataSource
     
-    init(remoteDataSource: CharacterRemoteDataSource) {
+    init(remoteDataSource: CharacterRemoteDataSource,
+         localDataSource: PersistenceDataSource) {
+        
         self.remoteDataSource = remoteDataSource
+        self.localDataSource = localDataSource
     }
     
     func fetchCharacter(page: Int) async throws -> PageResults<Character> {
         let response = try await remoteDataSource.fetchCharacter(page: page)
         let pageResult = response.toDomain(currentPage: page)
-
+        
+        #warning("TODO: need to save in local")
+        
         return pageResult
     }
     
     func localCharacters() async throws -> [Character] {
-#warning("TODO: need implement Persistence data base")
-        return []
+        try await localDataSource.getCharacters()
     }
 }
 
