cc44695ca1d0007953787b358b7bf82ad098e837
Omar Fazal <silen08620@gmail.com>
2026-05-04 00:01:43 +0200

    RM-4 - ✅ verify SwiftDataSource and Repository integration

--- RickAndMorty/Data/Repositories/RickAndMortyCharacterRepository.swift
+++ RickAndMorty/Data/Repositories/RickAndMortyCharacterRepository.swift
@@ -18,10 +18,11 @@ final class RickAndMortyCharacterRepository: CharacterRepository {
     }
     
     func fetchCharacter(page: Int) async throws -> PageResults<Character> {
+        
         let response = try await remoteDataSource.fetchCharacter(page: page)
         let pageResult = response.toDomain(currentPage: page)
         
-        #warning("TODO: need to save in local")
+        try await localDataSource.saveCharacters(pageResult.items)
         
         return pageResult
     }
