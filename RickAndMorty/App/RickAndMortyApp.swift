9c9c0e54a3ee90006554a65e8575f0c7734e09c1
Omar Fazal <silen08620@gmail.com>
2026-05-03 18:58:06 +0200

    RM-1 - restructure injitial project files

---
+++ RickAndMorty/App/RickAndMortyApp.swift
@@ -0,0 +1,19 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by silenGSR on 03/05/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RickAndMortyApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            TestContentView()
+        }
+    }
+}
---
+++ RickAndMorty/Feature/TestContent/TestContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by silenGSR on 03/05/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+struct TestContentView: View {
+
+    var body: some View {
+        Text("Test content view")
+    }
+}
+
+#Preview {
+    TestContentView()
+}
