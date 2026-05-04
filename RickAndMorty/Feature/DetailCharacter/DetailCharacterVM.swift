ef30794ee0a325cf86afb96d2f274ae068d20a6d
Omar Fazal <silen08620@gmail.com>
2026-05-04 22:49:02 +0200

    RM-8 - values to show displays from Character

--- RickAndMorty/Feature/DetailCharacter/DetailCharacterVM.swift
+++ RickAndMorty/Feature/DetailCharacter/DetailCharacterVM.swift
@@ -14,19 +14,40 @@ final class DetailCharacterVM {
     
     private var character: Character
     
-    var displayName: String { character.name }
-    var displayStatus: String { character.status.display }
-    var displayGender: String { character.gender.display }
     var displayImageURL: URL? { character.imageURL }
+    var displayName: String { character.name }
+    var displayStatus: String {
+        "chm_display_status".localized([character.status.display])
+    }
     var displaySpecies: String { character.displaySpecies }
     
+    var displayGender: String {
+        "chm_display_gender".localized([character.gender.display])
+    }
+    var displaySyncronizedAt: String {
+        "chm_display_syncronized_at".localized([character.displaySyncronized])
+    }
+    
+    var displayOriginLocation: String { character.origin.name }
+    var displayCurrentLocation: String { character.location.name}
+    
+    var displayCreatedAt: String {
+        "chm_display_created_at".localized([character.displayCreated])
+    }
+    
+    var displayParticipatedEpisodes: String { "chm_display_participated_episodes".localized([character.episodeIDs.count])
+    }
+    
+    
+    
     var needSyncFromServer: Bool {
-        #warning("TODO: for other version update character just to know if something has changes (episodes, status{dead},..)")
+#warning("TODO: for other version update character just to know if something has changes (episodes, status{dead},..)")
         let daysToUpdate: Int = 30
         let timeInterval = TimeInterval(daysToUpdate * 3600 * 3600 * 24)
-        
-       return character.syncronizedDate.addingTimeInterval(timeInterval) < Date()
-      }
+        // remove hardcode value when implementation are ready
+        return false
+        return character.syncronizedDate.addingTimeInterval(timeInterval) < Date()
+    }
     
     init(character: Character) {
         self.character = character
