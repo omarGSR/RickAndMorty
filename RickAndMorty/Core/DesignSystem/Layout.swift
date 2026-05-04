9e092b0643f0ba0f24d57a1bde70a5e5e08e4e8e
Omar Fazal <silen08620@gmail.com>
2026-05-04 12:44:17 +0200

    RM-6 - desing system color, spacing

---
+++ RickAndMorty/Core/DesignSystem/Layout.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled.swift
+//  RickAndMorty
+//
+//  Created by silenGSR on 04/05/2026.
+//
+
+import Foundation
+
+enum Spacing {
+    static let regular: CGFloat = 16
+    static let half: CGFloat = Spacing.regular * 0.5
+}
+
+enum Radius {
+    static let rectangular: CGFloat = 20
+}
