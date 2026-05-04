4f515f7e781c5cdf88e9a4276ce61c928b572db3
Omar Fazal <silen08620@gmail.com>
2026-05-04 21:54:42 +0200

    RM-8 - first structure UI

--- RickAndMorty/Feature/DetailCharacter/DetailCharacterVM.swift
+++ RickAndMorty/Feature/DetailCharacter/DetailCharacterVM.swift
@@ -5,18 +5,30 @@
 //  Created by silenGSR on 04/05/2026.
 //
 
+import Foundation
 import Observation
 
 @MainActor
 @Observable
 final class DetailCharacterVM {
     
-    var character: Character
+    private var character: Character
+    
+    var displayName: String { character.name }
+    var displayStatus: String { character.status.display }
+    var displayGender: String { character.gender.display }
+    var displayImageURL: URL? { character.imageURL }
+    var displaySpecies: String { character.displaySpecies }
+    
+    var needSyncFromServer: Bool {
+        #warning("TODO: for other version update character just to know if something has changes (episodes, status{dead},..)")
+        let daysToUpdate: Int = 30
+        let timeInterval = TimeInterval(daysToUpdate * 3600 * 3600 * 24)
+        
+       return character.syncronizedDate.addingTimeInterval(timeInterval) < Date()
+      }
     
     init(character: Character) {
         self.character = character
     }
-    
-    
-    
 }
--- RickAndMorty/Feature/DetailCharacter/DetailCharacterView.swift
+++ RickAndMorty/Feature/DetailCharacter/DetailCharacterView.swift
@@ -11,22 +11,106 @@ struct DetailCharacterView: View {
     
     @State var viewModel: DetailCharacterVM
     
+    let verticalSectionSpacing: CGFloat = Spacing.quarter
+    
     init(viewModel: DetailCharacterVM) {
         _viewModel = State(initialValue: viewModel)
     }
     
     var body: some View {
         ScrollView {
-            RemoteImageView(url: viewModel.character.imageURL)
-                .frame(width: 150, height: 150)
-                .clipShape(RoundedRectangle(cornerRadius: Radius.card))
-            Text(viewModel.character.name)
+            
+            header
+            sectionInfo
+            sectionLocation
+                .padding(.top, -Spacing.regular)
+            sectionSync
+                .padding(.top, -Spacing.regular)
+            
             Spacer()
-            
-            
         }
     }
     
+    private var header: some View {
+        VStack(spacing: verticalSectionSpacing) {
+            
+            RemoteImageView(url: viewModel.displayImageURL)
+                .frame(width: 150, height: 150)
+                .clipShape(RoundedRectangle(cornerRadius: Radius.card))
+            
+            Text(viewModel.displayName)
+                .titleItemStyle()
+                .padding(.top, Spacing.regular)
+            
+            Text(viewModel.displaySpecies)
+                .descriptionItemStyle(color: .primary)
+            
+            Text("chm_display_status".localized([viewModel.displayStatus]))
+                .descriptionItemStyle()
+        }
+        .padding(Spacing.regular)
+    }
+    
+    private var sectionInfo: some View {
+        
+        GroupBox {
+            VStack(alignment: .leading, spacing: verticalSectionSpacing) {
+                
+                Text("_gender: Male")
+                Text("_participate in 88 episodes")
+                Text("_create at: Juny 20226")
+            }
+            .padding(.leading, Spacing.regular)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+        } label : {
+            Text("_info")
+        }
+        .padding(Spacing.regular)
+    }
+    
+    private var sectionLocation: some View {
+        
+        GroupBox {
+            VStack(alignment: .leading, spacing: verticalSectionSpacing) {
+                
+                Text("_original from:")
+                Text("unknow")
+                    .frame(maxWidth: .infinity, alignment: .center)
+                Text("_current:")
+                Text("unknow")
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            .padding(.leading, Spacing.regular)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+        } label : {
+            Text("_location")
+            
+        }
+        .padding(Spacing.regular)
+    }
+    
+    private var sectionSync: some View {
+        GroupBox {
+            VStack(alignment: .center, spacing: verticalSectionSpacing) {
+                
+                Spacer()
+                Button("sync") {
+                    
+                }
+                .disabled(!viewModel.needSyncFromServer)
+                .buttonStyle(.borderedProminent)
+            }
+            .padding(.leading, Spacing.regular)
+            
+        } label : {
+            Text("_last syncronized 23/5/20026")
+            
+        }
+        .padding(Spacing.regular)
+        
+    }
 }
 
 
