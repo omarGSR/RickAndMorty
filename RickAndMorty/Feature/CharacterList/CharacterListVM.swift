4dc0c82dfb134b2c735d2702a801708bf57e4733
Omar Fazal <silen08620@gmail.com>
2026-05-04 20:08:12 +0200

    RM-7 - viewmodel necesary updates to handler pagination

--- RickAndMorty/Feature/CharacterList/CharacterListVM.swift
+++ RickAndMorty/Feature/CharacterList/CharacterListVM.swift
@@ -20,16 +20,53 @@ final class CharacterListVM {
         case errorStateView // includes not internet
     }
     
+    private enum FetchPageContext {
+        case initial
+        case next
+    }
+    
     var characters: [Character] = []
     var stateView: CharacterListState = .idle
     
-    @ObservationIgnored
-    var errorFirstEvaluate: Error?
-    
     private var characterRepository: CharacterRepository
+    private let networkMonitor: NetworkMonitoring
     
-    init(characterRepository: CharacterRepository) {
+    @ObservationIgnored var errorFirstEvaluate: Error?
+    var errorAlert: Error?
+    
+    var forceShowButtonLoadMore: Bool = false
+    
+    var isConectionReachable: Bool {
+        networkMonitor.isConnected
+    }
+    
+    // MARK: - About pages
+    
+    var isFetchingNextPage = false
+    
+    @ObservationIgnored private var pageInfo: PageInfo?
+   
+    private var nextPage: Int? {
+        guard let pageInfo else {
+            return characters.isEmpty ? 1 : nil
+        }
+        return pageInfo.nextPage
+    }
+    
+    var currentPage: Int {
+        nextPage ?? 1
+    }
+    
+    var isPaginationAvailable: Bool {
+        nextPage != nil
+    }
+
+    // MARK: - Init
+    
+    init(characterRepository: CharacterRepository,
+         networkMonitor: NetworkMonitoring) {
         self.characterRepository = characterRepository
+        self.networkMonitor = networkMonitor
     }
     
     func loadInitialValues(isRetry: Bool = false) async {
@@ -42,9 +79,10 @@ final class CharacterListVM {
         
         do {
             characters = try await characterRepository.localCharacters()
+            pageInfo = try await characterRepository.localCharactersPageInfo()
             
             if characters.isEmpty {
-                await fetchFirstRemotePage()
+                await fetchRemotePage(context: .initial)
             } else {
                 stateView = .showList
             }
@@ -54,15 +92,59 @@ final class CharacterListVM {
         }
     }
     
-    private func fetchFirstRemotePage() async {
-        do {
+    // MARK: - Fetch data
+    
+    func fetchNextRemotePage() async {
+        await fetchRemotePage(context: .next)
+    }
+    
+    private func fetchRemotePage(context: FetchPageContext) async {
+        
+        guard let nextPage else { return }
+        guard !isFetchingNextPage else { return }
+        
+        isFetchingNextPage = true
+        forceShowButtonLoadMore = false
+        
+        switch context {
+        case .initial:
             stateView = .fetchFirstRemote
-            let pageResults = try await characterRepository.fetchCharacter(page: 1)
-            characters = pageResults.items
-            stateView = .showList
+            
+        case .next:
+            break
+        }
+        
+        defer {
+            isFetchingNextPage = false
+        }
+        
+        do {
+            let pageResults = try await characterRepository.fetchCharacter(page: nextPage)
+            
+            switch context {
+            case .initial:
+                characters = pageResults.items
+                stateView = .showList
+                
+            case .next:
+                let savedIDs = Set(characters.map(\.id))
+                let newItems = pageResults.items.filter { !savedIDs.contains($0.id) }
+                characters.append(contentsOf: newItems)
+            }
+            
+            pageInfo = pageResults.pageInfo
+            
         } catch {
-            errorFirstEvaluate = error
-            stateView = .errorStateView
+            switch context {
+                
+            case .initial:
+                errorFirstEvaluate = error
+                stateView = .errorStateView
+                
+            case .next:
+                forceShowButtonLoadMore = true
+                errorAlert = error
+            }
         }
     }
 }
