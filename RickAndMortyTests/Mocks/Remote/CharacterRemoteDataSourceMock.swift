764d9796d54cc4de428cdc12cfa3899a244485d5
Omar Fazal <silen08620@gmail.com>
2026-05-05 09:46:06 +0200

    RM-11 - update existing mocks

--- RickAndMortyTests/Mocks/Remote/CharacterRemoteDataSourceMock.swift
+++ RickAndMortyTests/Mocks/Remote/CharacterRemoteDataSourceMock.swift
@@ -10,14 +10,25 @@
 final class CharacterRemoteDataSourceMock: CharacterRemoteDataSource {
     
     private let response: Result<CharactersResponseDTO, Error>
+    private let characterResponse: Result<CharacterDTO, Error>
     private(set) var receivedPages: [Int] = []
+    private(set) var receivedIDs: [Int] = []
     
-    init(response: Result<CharactersResponseDTO, Error>) {
+    init(
+        response: Result<CharactersResponseDTO, Error>,
+        characterResponse: Result<CharacterDTO, Error> = .success(.mock())
+    ) {
         self.response = response
+        self.characterResponse = characterResponse
     }
     
     func fetchCharacter(page: Int) async throws -> CharactersResponseDTO {
         receivedPages.append(page)
         return try response.get()
+    }
+    
+    func fetchCharacter(id: Int) async throws -> CharacterDTO {
+        receivedIDs.append(id)
+        return try characterResponse.get()
     }
 }
